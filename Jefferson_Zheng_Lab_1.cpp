b8b9a19037f5edbb3a16e1b599372d2877c13eb9
MythicalPhantom <158225349+MythicalPhantom@users.noreply.github.com>
2024-09-10 16:51:03 -0400

    Create Jefferson_Zheng_Lab_1.cpp

---
+++ Jefferson_Zheng_Lab_1.cpp
@@ -0,0 +1,37 @@
+//Jefferson Zheng
+//Lab 1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "######################################################################################" << endl;
+  cout << "#                                                                                    #" << endl;
+  cout << "# Ways to access the Task Manager on your Windows computer:                          #" << endl;
+  cout << "#                                                                                    #" << endl;
+  cout << "#            Press the key combination Ctrl + Shift + Escape                         #" << endl;
+  cout << "#                                                                                    #" << endl;
+  cout << "#            Press the key combination Ctrl + Alt + Delete and select \"Task Manager\" #" << endl;
+  cout << "#                                                                                    #" << endl;
+  cout << "#            Type \"Task Manager\" in the Windows Start menu search                    #" << endl;
+  cout << "#                                                                                    #" << endl;
+  cout << "######################################################################################" << endl;
+  return 0;
+}
+
+/*
+[Running] cd "c:\Users\jefferson.zheng23\Documents\GitHub\Test\" && g++ test_2.cpp -o test_2 && "c:\Users\jefferson.zheng23\Documents\GitHub\Test\"test_2
+######################################################################################
+#                                                                                    #
+# Ways to access the Task Manager on your Windows computer:                          #
+#                                                                                    #
+#            Press the key combination Ctrl + Shift + Escape                         #
+#                                                                                    #
+#            Press the key combination Ctrl + Alt + Delete and select "Task Manager" #
+#                                                                                    #
+#            Type "Task Manager" in the Windows Start menu search                    #
+#                                                                                    #
+######################################################################################
+
+[Done] exited with code=0 in 1.509 seconds
+*/
